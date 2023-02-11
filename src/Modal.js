0113f34c2e86a7946a30288bd4643b4f6d87dedc
GrantCanty <gc933815@ohio.edu>
2023-02-10 23:56:53 -0700

    changed css

--- src/Modal.js
+++ src/Modal.js
@@ -12,69 +12,72 @@ const Modal = ({show, onCloseModalButton, resorts, submitForm, handleOptionChang
        <div className='modal-bg'>
             <div className='modal'>
                 <form onSubmit={submitForm}>
-                    <label>Modify: 
-                        <select 
-                        name="option"
-                        value={option}
-                        onChange={handleOptionChange}
-                        >
-                            <option value="" disabled={true}>-- Choose an Option --</option>
-                            <option value="Add">Add</option>
-                            <option value="Delete">Delete</option>
-                        </select>
-                    </label>
-                    {option === "" ? null :
-                        option === "Add" ?
-                            <div>
-                                <label>Resort Name 
-                                    <input 
-                                    key="name"
-                                    type="text" 
-                                    name="resortName"
-                                    value={form.resortName}
-                                    onChange={handleFormChange}
-                                    />
-                                </label>
-                                <br></br>
-                                <label>Longitude
-                                    <input 
-                                    key="long"
-                                    type="number" 
-                                    name="longitude"
-                                    value={form.longitude}
-                                    onChange={handleFormChange}
-                                    />
-                                </label>
-                                <br></br>
-                                <label>Latitude
-                                    <input 
-                                    key="lat"
-                                    type="number" 
-                                    name="latitude"
-                                    value={form.latitude}
-                                    onChange={handleFormChange}
-                                    />
-                                </label>
-                                <button type='submit'>Submit</button>
-                            </div> :
-                            <div>
-                                <label>Delete
-                                    <select 
-                                    name="resortName"
-                                    value={form.resortName}
-                                    onChange={handleFormChange}
-                                    >   
-                                    <option value="" disabled={true}>-- Delete any Resort --</option>
-                                        {[...resorts.keys()].map((item)=> {
-                                            return <option key={item} value={item}>{item}</option>
-                                        })}
-                                    </select>
-                                </label>
-                                <button type='submit'>Submit</button>
-                            </div>
-                    }
+                    <div className='form-body'>
+                    <label className='form-title'>Modify Resort List</label>
+                        <label>Option: 
+                            <select 
+                            name="option"
+                            value={option}
+                            onChange={handleOptionChange}
+                            >
+                                <option value="" disabled={true}>-- Choose an Option --</option>
+                                <option value="Add">Add</option>
+                                <option value="Delete">Delete</option>
+                            </select>
+                        </label>
+                        {option === "" ? null :
+                            option === "Add" ?
+                                <div>
+                                    <label>Resort Name 
+                                        <input 
+                                        key="name"
+                                        type="text" 
+                                        name="resortName"
+                                        value={form.resortName}
+                                        onChange={handleFormChange}
+                                        />
+                                    </label>
+                                    <label>Longitude
+                                        <input 
+                                        key="long"
+                                        type="number" 
+                                        name="longitude"
+                                        value={form.longitude}
+                                        onChange={handleFormChange}
+                                        />
+                                    </label>
+                                    <label>Latitude
+                                        <input 
+                                        key="lat"
+                                        type="number" 
+                                        name="latitude"
+                                        value={form.latitude}
+                                        onChange={handleFormChange}
+                                        />
+                                    </label>
+                                    <button type='submit'>Submit</button>
+                                </div> :
+                                <div>
+                                    <label>Delete Resort
+                                        <select 
+                                        name="resortName"
+                                        value={form.resortName}
+                                        onChange={handleFormChange}
+                                        >   
+                                        <option value="" disabled={true}>-- Delete any Resort --</option>
+                                            {[...resorts.keys()].map((item)=> {
+                                                return <option key={item} value={item}>{item}</option>
+                                            })}
+                                        </select>
+                                    </label>
+                                    <button type='submit'>Submit</button>
+                                </div>
+                        }
+                    </div>
                 </form>
-                <button onClick={onCloseModalButton}>Close Modal</button>
+                <div className='submit'>
+                    <button onClick={onCloseModalButton}>Close Modal</button>
+                </div>
             </div>
         </div>
         , document.body
